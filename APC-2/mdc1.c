99915fd7c0cbede4e9110a54d23f9dbdfddf1543
Thiago <thiagozoid@gmail.com>
2018-08-15 08:59:37 -0300

    Update mdc1.c

--- APC-2/mdc1.c
+++ APC-2/mdc1.c
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-                                              // Função de MDC
+                                              	    // Função de MDC
 int mdc(int a, int b){
 	int i,j,k=1;                                // i = Contador; j = Maior número entre "a" e "b"; k = Resultado
 	if (a>b){j=a;}else{j=b;}                    // Define o maior número
 	for(i=1;i<j;i++){                           // Repete a tentativa de divisão até "j".
-		if((a%i==0)&&(b%i==0)){                   // Caso seja um divisor comum, é válido para o MDC
-			k*=i;                                   // Ele é multiplicado pelo resultado
+		if((a%i==0)&&(b%i==0)){             // Caso seja um divisor comum, é válido para o MDC
+			k*=i;                       // Ele é multiplicado pelo resultado
 		}
 	}
 	return k;                                   // A função retorna o resultado
 }
 
-                                              // Função principal
+                                              	    // Função principal
 main(){
 	int a,b;                                    // Dois números em questão
 	printf("Digite dois numeros (x y):");       // Diálogo simples
